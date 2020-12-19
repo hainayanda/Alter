d31456e8bfdf5cc3e90f2e9257d6ca2f7bef606f
Nayanda Haberty <nayanda1@outlook.com>
2020-12-19 20:35:14 +0700

    Update test_lib.sh

--- .github/scripts/test_lib.sh
+++ .github/scripts/test_lib.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace Example/Alter.xcworkspace \
             -scheme Alter-Example \
-            -destination platform=iOS\ Simulator,OS=14.3,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
